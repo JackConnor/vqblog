874ad8bd642b90f16b10628f1560209dde00d9d2
Jack Connor <jack.connor83@gmail.com>
2015-11-30 16:47:02 -0800

    acompleted v1 of hover effect

--- public/javascripts/controllers/mainPageController.js
+++ public/javascripts/controllers/mainPageController.js
@@ -6,7 +6,44 @@ var app = angular.module('mainPageController', [])
 
   function mainPageCtrl($http){
     var self = this;
-    
+
+    /////////angular events to do the hover effect
+    //////////////////////////////////////////////
+    /////////////////////////////////////////////
+    var hoverCounterArray = [];
+
+    self.hoverArticle = function(ind){
+      if(!hoverCounterArray[ind]){
+        $('#homeArticle'+ind).prepend(
+          "<div class='linkHolderContainer' id='linkHolderContainer"+ind+"'>"+
+          "</div>"
+        )
+        $('#linkHolderContainer'+ind).prepend(
+          "<div class='linkHolder' id='linkHolder"+ind+"'>"+
+            "<h2>Read Full Article</h2>"+
+          "</div>"
+        )
+        $('#linkHolder'+ind).on('mouseenter', function(){
+          $('#linkHolder'+ind).css({
+            backgroundColor: "#730E5F"
+          })
+        })
+        $('#linkHolder'+ind).on('mouseleave', function(){
+          $('#linkHolder'+ind).css({
+            backgroundColor: "#4B1240"
+          })
+        })
+        hoverCounterArray[ind] = !hoverCounterArray[ind];
+      } else {
+        $('#linkHolder'+ind).remove();
+        $('#linkHolderContainer'+ind).remove();
+        hoverCounterArray[ind] = !hoverCounterArray[ind];
+      }
+    }
+
+    /////////end angular events to do the hover effect
+    //////////////////////////////////////////////////
+    //////////////////////////////////////////////////
 
     ///add in seed file, file swap out later for database
     self.seed = [
